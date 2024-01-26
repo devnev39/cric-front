90c1f077d3827655ee70b32ed362cdd0f5e30db1
Dhruv Bonde <devnev39@gmail.com>
2024-01-26 12:01:57 +0530

    refactor: refactored helpers and config #4

--- src/helpers/fetchData.jsx
+++ src/helpers/fetchData.jsx
@@ -1,11 +1,11 @@
-export default async function fetchData(uri,body){
-    const req = {
-        method : "POST",
-        headers : {
-            "Content-Type" : "application/json"
-        },
-        body : JSON.stringify(body),
-        credentials : "include"
-    }
-    return await (await fetch(uri,req)).json();
-}
+export default fetchData = async (uri, body) => {
+  const req = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+    credentials: 'include',
+  };
+  return await (await fetch(uri, req)).json();
+};
