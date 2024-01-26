8677e7d2b3063fd9163534413d1295c3e68bd09d
Dhruv Bonde <devnev39@gmail.com>
2024-01-26 11:10:01 +0530

    chore: Migrate to vite #3

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.VITE_ENV': JSON.stringify(env.VITE_ENV),
+      'process.env.VITE_ENKEY': JSON.stringify(env.VITE_ENKEY)
+    },
+    plugins: [react()],
+  }
+})
