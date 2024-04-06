6aec050570aded6d636b9eac9b52a90d3187e718
Dhruv Bonde <devnev39@gmail.com>
2024-04-06 23:45:37 +0530

    chore: added about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,32 +1,183 @@
 import React from "react";
-import "./styles.css";
 import Footer from "../Footer";
+import {
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBTypography,
+  MDBIcon,
+  MDBCard,
+  MDBCardBody,
+} from "mdb-react-ui-kit";
+import ss1 from "../../resources/ss1.png";
+import ss2 from "../../resources/ss2.png";
+import ReactIcon from "../../resources/react.svg?react";
+import MdbIcon from "../../resources/mdb.svg?react";
+import ReduxIcon from "../../resources/redux.svg?react";
+import DockerIcon from "../../resources/docker.svg?react";
+import MongoIcon from "../../resources/mongo.svg?react";
+import VercelIcon from "../../resources/vercel.svg?react";
 
 const About = () => {
   return (
     <>
-      <div className="row m-2">
-        <div className="col-5 p-5 h3 lh-base">
-          Cric is a revolutionary platform that puts you in control, allowing
-          you to simulate, manage, and administer cricket auctions with ease.
-          Optimized for the dynamic world of cricket, Cric gives you the tools
-          to draft your dream team and strategize for victory.
-        </div>
-        <div className="col-7 d-flex justify-content-center align-items-center">
-          <img className="img-class" alt="SS1" src="/ss1.png" />
-        </div>
-      </div>
-      <div className="row m-2 mt-5">
-        <div className="col-7 d-flex justify-content-center align-items-center">
-          <img className="img-class" alt="SS2" src="/ss2.png" />
-        </div>
-        <div className="col-5 p-5 h3 lh-base">
-          Cric empowers you to create custom rules that tailor team shortlisting
-          to your exact specifications.
-        </div>
-      </div>
-      <div className="row m-2 mt-5 d-flex justify-content-center">
-        For any queries - dhruvbonde007@gmail.com
+      <div style={{ minHeight: "75vh", marginTop: "1rem" }}>
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol size={6}>
+              <MDBCard>
+                <MDBCardBody>
+                  <MDBTypography className="display-6">
+                    <MDBTypography tag={"mark"}>Streamlined</MDBTypography>
+                    Cricket Auction Management
+                  </MDBTypography>
+                  <MDBContainer>
+                    <MDBRow>
+                      <MDBCol size={1}>
+                        <MDBIcon icon="check-circle" className="text-success" />
+                      </MDBCol>
+                      <MDBCol>
+                        <MDBTypography blockquote>
+                          Run multiple auctions with the same player pool or
+                          easily adjust player data for each event.
+                        </MDBTypography>
+                      </MDBCol>
+                    </MDBRow>
+                    <MDBRow>
+                      <MDBCol size={1}>
+                        <MDBIcon icon="check-circle" className="text-success" />
+                      </MDBCol>
+                      <MDBCol>
+                        <MDBTypography blockquote>
+                          Manage teams and player information efficiently,
+                          saving you time and effort.
+                        </MDBTypography>
+                      </MDBCol>
+                    </MDBRow>
+                  </MDBContainer>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+            <MDBCol size={6}>
+              <MDBCard>
+                <MDBCardBody>
+                  <img style={{ width: "40vw" }} src={ss2} />
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+        <hr className="hr" />
+        <MDBContainer className="mt-3">
+          <MDBRow>
+            <MDBCol size={6}>
+              <MDBCard>
+                <MDBCardBody>
+                  <img style={{ width: "40vw" }} src={ss1} />
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+            <MDBCol size={6}>
+              <MDBCard>
+                <MDBCardBody>
+                  <MDBTypography className="display-6">
+                    <MDBTypography tag={"mark"}>Real-Time</MDBTypography>
+                    Transparency and Engagement
+                  </MDBTypography>
+                  <MDBContainer>
+                    <MDBRow>
+                      <MDBCol size={1}>
+                        <MDBIcon icon="check-circle" className="text-success" />
+                      </MDBCol>
+                      <MDBCol>
+                        <MDBTypography blockquote>
+                          Deliver live data updates directly to teams, ensuring
+                          everyone stays informed throughout the auction.
+                        </MDBTypography>
+                      </MDBCol>
+                    </MDBRow>
+                    <MDBRow>
+                      <MDBCol size={1}>
+                        <MDBIcon icon="check-circle" className="text-success" />
+                      </MDBCol>
+                      <MDBCol>
+                        <MDBTypography blockquote>
+                          Foster a transparent and engaging experience for all
+                          participants.
+                        </MDBTypography>
+                      </MDBCol>
+                    </MDBRow>
+                  </MDBContainer>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+        <hr className="hr" />
+        <MDBContainer className="mt-3">
+          <MDBRow>
+            <MDBCol size={6}>
+              <MDBCard>
+                <MDBCardBody>
+                  <MDBTypography className="display-6">
+                    <MDBTypography tag={"mark"}>Secure</MDBTypography>
+                    and
+                    <MDBTypography tag={"mark"}>Reliable</MDBTypography>
+                    Platform
+                  </MDBTypography>
+                  <MDBContainer>
+                    <MDBRow>
+                      <MDBCol size={1}>
+                        <MDBIcon icon="check-circle" className="text-success" />
+                      </MDBCol>
+                      <MDBCol>
+                        <MDBTypography blockquote>
+                          Conduct your auctions with confidence thanks to secure
+                          login and robust session management.
+                        </MDBTypography>
+                      </MDBCol>
+                    </MDBRow>
+                  </MDBContainer>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+            <MDBCol size={6}>
+              <MDBCard>
+                <MDBCardBody>
+                  <img style={{ width: "40vw" }} src={ss1} />
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+        <hr className="hr" />
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol>
+              <MDBTypography className="display-5">
+                Technologies Used
+              </MDBTypography>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow>
+            <MDBCol>
+              <div className="d-flex justify-content-evenly">
+                <ReactIcon style={{ height: "10vh" }} />
+                <MdbIcon style={{ height: "10vh" }} />
+                <ReduxIcon style={{ height: "10vh" }} />
+                <MongoIcon style={{ height: "10vh" }} />
+                {/* <div>
+                  <MDBTypography className="display-6">Express</MDBTypography>
+                </div> */}
+                <DockerIcon style={{ height: "10vh" }} />
+                {/* <div>
+                  <MDBTypography className="display-6">Render</MDBTypography>
+                </div> */}
+                <VercelIcon style={{ height: "10vh" }} />
+              </div>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
       </div>
       <Footer />
     </>
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import {defineConfig, loadEnv} from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
   return {
     define: {
-      'process.env.VITE_ENV': JSON.stringify(env.VITE_ENV),
-      'process.env.VITE_ENKEY': JSON.stringify(env.VITE_ENKEY),
+      "process.env.VITE_ENV": JSON.stringify(env.VITE_ENV),
+      "process.env.VITE_ENKEY": JSON.stringify(env.VITE_ENKEY),
     },
-    plugins: [react()],
+    plugins: [react(), svgr()],
   };
 });
